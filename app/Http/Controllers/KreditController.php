b09afdf591c36a72ec2fbf05943a36ad9971dc0c
Ida Bagus Putu Putra Manuaba <ibp.putra.m@gmail.com>
2024-11-28 02:19:50 +0700

    Gilig Fix Berhasil

--- app/Http/Controllers/KreditController.php
+++ app/Http/Controllers/KreditController.php
@@ -80,12 +80,11 @@ class KreditController extends Controller
             $hutangPlusBunga = $kredit->jumlah + $bunga;
 
 
-            // Cek apakah created_at atau updated_at null atau tanggalnya sama
-            if (!$kredit->created_at || !$kredit->update_at || $kredit->created_at->eq($kredit->update_at)) {
+            $diffInMonthsUpdate = $kreditDate->diffInMonths($kredit->update_at);
+
+            // Cek apakah tanggal created_at dan updated_at sama
+            if ($kredit->created_at->eq($kredit->updated_at)) {
                 $diffInMonthsUpdate = 0;
-            } else {
-                // Jika tidak null dan tanggalnya berbeda, hitung selisih bulan
-                $diffInMonthsUpdate = $kreditDate->diffInMonths($kredit->update_at);
             }
 
             // Pastikan perbedaan bulan menjadi negatif dan dibulatkan ke bawah
