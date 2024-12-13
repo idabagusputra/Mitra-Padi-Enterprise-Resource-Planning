84b466b204a2cbb6b7b676c1b650e32df1e20f8d
Ida Bagus Putu Putra Manuaba <ibp.putra.m@gmail.com>
2024-12-14 00:57:33 +0700

    Update Field Cari Petani

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -24,7 +24,7 @@ class SessionsController extends Controller
         // return redirect()->route('giling.index');
         // Cek jika sudah login, langsung arahkan ke halaman giling
         if (auth() && Auth::check()) {
-            return redirect()->route('giling.index');
+            return redirect()->route('kredit.index');
         }
 
         try {
@@ -42,7 +42,7 @@ class SessionsController extends Controller
                 session()->forget('_token');
                 session()->put('_token', csrf_token());
 
-                return redirect()->intended(route('giling.index'));
+                return redirect()->intended(route('kredit.index'));
             }
 
             return back()->withErrors(['email' => 'Email or password invalid.']);
