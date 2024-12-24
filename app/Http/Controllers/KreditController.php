157894c0d1f223f626af52aa61db0bb31d85c247
Ida Bagus Putu Putra Manuaba <ibp.putra.m@gmail.com>
2024-12-24 18:34:16 +0700

    Scroll Bar dan Sidebar click outside

--- app/Http/Controllers/KreditController.php
+++ app/Http/Controllers/KreditController.php
@@ -236,6 +236,9 @@ class KreditController extends Controller
             // Ambil data validasi
             $validatedData = $validator->validated();
 
+            // Transformasi keterangan untuk menjadikan huruf awal setiap kata kapital
+            $validatedData['keterangan'] = ucwords(strtolower($validatedData['keterangan']));
+
             // Konversi tanggal ke format timestamp
             $timestamp = Carbon::createFromFormat('Y-m-d', $validatedData['tanggal'])->toDateTimeString();
 
