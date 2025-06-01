a915f18f561faa7356c24d3c669d512c1ddca79f
Ida Bagus Putu Putra Manuaba <ibp.putra.m@gmail.com>
2025-06-01 10:47:00 +0700

    Update Thermal

--- resources/views/receipt/thermal.blade.php
+++ resources/views/receipt/thermal.blade.php
@@ -404,7 +404,7 @@
             </tr> -->
 
             <tr class="total">
-                <td>Total Pengambilan</td>
+                <td>T. Pengambilan</td>
                 <td>:</td>
                 <td>Rp {{ number_format($daftar->total_pengambilan, 2) }}</td>
             </tr>
