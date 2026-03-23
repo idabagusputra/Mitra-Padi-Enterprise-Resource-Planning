496a7a2fb75f32b6ea083950d58299f56b63b38c
Ida Bagus Putu Putra Manuaba <ibp.putra.m@gmail.com>
2026-03-23 22:23:19 +0700

    revisi save nota

--- resources/views/nota-sementara.blade.php
+++ resources/views/nota-sementara.blade.php
@@ -1486,7 +1486,7 @@ biayaBuruhJemur: (jemurMode ?? 'N') === 'D' ? 10000 : 8000,
         await new Promise(resolve => setTimeout(resolve, 800));
 
         const canvas = await html2canvas(notaElement, {
-            scale: 5,
+            scale: 3,
             useCORS: true,
             allowTaint: true,
             backgroundColor: '#ffffff',
