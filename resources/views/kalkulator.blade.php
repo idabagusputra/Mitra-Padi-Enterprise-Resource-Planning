b976b63d145239d1e8a85af0c8aa3cac4d0674af
Ida Bagus Putu Putra Manuaba <ibp.putra.m@gmail.com>
2026-03-30 22:12:18 +0700

    revisi nota harga rata

--- resources/views/kalkulator.blade.php
+++ resources/views/kalkulator.blade.php
@@ -3518,17 +3518,17 @@ function generateRataRataHTML(calculatorType) {
 
   <hr class="sep" style="margin:2mm 0;">
 
-  <div style="font-size:11px;font-weight:900;margin-bottom:1.5mm;">Cara Hitung Rata" Harga Penjualan Beras :</div>
+  <div style="font-size:11px;font-weight:900;margin-bottom:1.5mm;">Cara Hitung Rata" Harga Jual Beras :</div>
 
-  <div style="font-size:11px;line-height:1.8;background:#f9f9f9;padding:2mm 2mm;border-left:2.5px solid #000;margin-bottom:1mm;font-weight:700;">
+  <div style="font-size:11px;line-height:1.8;background:#f9f9f9;padding:2mm 2mm;border-left:2.5px solid #000;margin-bottom:1mm;font-weight:1000;">
     <div style="margin-bottom:1mm;">
       <strong>= Total Nilai / Total Berat</strong>
     </div>
-    <div style="opacity:.85;font-weight:700;">
+    <div style="opacity:.85;font-weight:1000;">
       = Rp ${formatRibuan(totalNilai.toFixed(0))} / ${formatRibuan(totalJumlahKg.toFixed(0))} Kg
     </div>
     <div style="margin-top:1mm;opacity:.85;">
-      = <strong style="font-size:12px;font-weight:900;">Rp ${formatRibuan(rataRata.toFixed(0))} / Kg</strong>
+      = <strong style="font-size:12px;font-weight:1000;">Rp ${formatRibuan(rataRata.toFixed(0))} / Kg</strong>
     </div>
   </div>
 
