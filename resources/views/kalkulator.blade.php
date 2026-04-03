23a88a4dbd552fa4b730b25793bfc04a6512c81a
Ida Bagus Putu Putra Manuaba <ibp.putra.m@gmail.com>
2026-04-04 06:13:46 +0700

    revisi nkal

--- resources/views/kalkulator.blade.php
+++ resources/views/kalkulator.blade.php
@@ -3427,14 +3427,7 @@ async function printLangsungRata(calculatorType) {
         <div style="text-transform: none !important; font-weight: normal !important;">.</div>
         <div style="text-transform: none !important; font-weight: normal !important;">.</div>
     </div>
-    <div class="waktu" style="display: flex; justify-content: space-between;">
-        <div style="text-transform: none !important; font-weight: normal !important;">.</div>
-        <div style="text-transform: none !important; font-weight: normal !important;">.</div>
-    </div>
-    <div class="waktu" style="display: flex; justify-content: space-between;">
-        <div style="text-transform: none !important; font-weight: normal !important;">.</div>
-        <div style="text-transform: none !important; font-weight: normal !important;">.</div>
-    </div>
+
         </html>
     `;
 
