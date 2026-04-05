5c60435ecb06350b5dddae5e1b87b884a73a697f
Ida Bagus Putu Putra Manuaba <ibp.putra.m@gmail.com>
2026-04-05 13:03:05 +0700

    revisi nkal

--- resources/views/kalkulator.blade.php
+++ resources/views/kalkulator.blade.php
@@ -3592,7 +3592,7 @@ function generateRataRataHTML(calculatorType) {
 
   <div style="font-size:11px;font-weight:900;margin-bottom:1.5mm;">Cara Hitung Rata" Harga Jual Beras :</div>
 
-  <div style="font-size:20px;line-height:1.8;background:#f9f9f9;padding:2mm 2mm;border-left:2.5px solid #000;margin-bottom:1mm;font-weight:1000;">
+  <div style="font-size:14px;line-height:1.8;background:#f9f9f9;padding:2mm 2mm;border-left:2.5px solid #000;margin-bottom:1mm;font-weight:1000;">
     <div style="margin-bottom:1mm;">
       <strong>= Total Nilai ÷ Total Berat</strong>
     </div>
@@ -3600,7 +3600,7 @@ function generateRataRataHTML(calculatorType) {
       = Rp ${formatRibuan(totalNilai.toFixed(0))} ÷ ${formatRibuan(totalJumlahKg.toFixed(0))} Kg
     </div>
     <div style="margin-top:1mm;opacity:.85;">
-      = <strong style="font-size:12px;font-weight:1000;">Rp ${formatRibuan(rataRata.toFixed(0))} / Kg</strong>
+      = <strong style="font-size:15px;font-weight:1000;">Rp ${formatRibuan(rataRata.toFixed(0))} / Kg</strong>
     </div>
   </div>
 
