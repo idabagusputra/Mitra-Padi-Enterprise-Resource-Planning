6340f6890267bab1b7191c09b0a83bd9677d1c13
Ida Bagus Putu Putra Manuaba <ibp.putra.m@gmail.com>
2026-04-05 19:25:16 +0700

    revisi nkal

--- resources/views/kalkulator.blade.php
+++ resources/views/kalkulator.blade.php
@@ -957,7 +957,7 @@
     </button>
 
 
-    <button class="btn btn-print half" onclick="saveRataRataHargaGabah('jumlah')" style="background-color: #0097A7; margin: 0 !important;">
+    <button class="btn btn-print half" onclick="saveRataRataHargaGabah('jumlah')" style="background-color: #0097A7;">
     <i class="fas fa-calculator"></i>
 </button>
 
@@ -966,7 +966,7 @@
     <i class="fas fa-file-invoice-dollar"></i>
 </button>
 
-<button class="btn btn-print half" onclick="saveRataRataHarga('jumlah')" style="background-color: #0097A7; margin: 0 !important;">
+<button class="btn btn-print half" onclick="saveRataRataHarga('jumlah')" style="background-color: #0097A7;">
     <i class="fas fa-tractor"></i>
 </button>
 
@@ -975,7 +975,7 @@
     <i class="fas fa-file-invoice-dollar"></i>
 </button>
 
-        <button class="btn btn-print half"  style="margin: 0 !important;"> onclick="saveNotaAsJPG('jumlah')">
+        <button class="btn btn-print half" onclick="saveNotaAsJPG('jumlah')" style="padding-right: 0;">>
     <i class="fas fa-download"></i>
 </button>
 
