08e755535f6c3b57a0600b06b0fe83e90ee6ad18
Ida Bagus Putu Putra Manuaba <ibp.putra.m@gmail.com>
2026-05-09 06:59:24 +0700

    but edit kredit

--- app/Http/Controllers/KreditTitipanPetaniController.php
+++ app/Http/Controllers/KreditTitipanPetaniController.php
@@ -253,11 +253,11 @@ class KreditTitipanPetaniController extends Controller
             // Simpan model ke database
             $kredit->save();
 
-            return response()->json([
-                'success' => true,
-                'message' => 'Kredit berhasil ditambahkan',
-                'data' => $kredit,
-            ]);
+            // return response()->json([
+            //     'success' => true,
+            //     'message' => 'Kredit berhasil ditambahkan',
+            //     'data' => $kredit,
+            // ]);
         } catch (\Exception $e) {
             Log::error('Error creating kredit: ' . $e->getMessage());
             return response()->json(['success' => false, 'message' => 'Terjadi kesalahan saat menambahkan kredit'], 500);
