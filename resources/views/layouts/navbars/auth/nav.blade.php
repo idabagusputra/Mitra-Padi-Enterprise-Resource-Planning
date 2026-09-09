0d2bc9a2dedcc527344fb4e61bf968b20a11ae76
Ida Bagus Putu Putra Manuaba <ibp.putra.m@gmail.com>
2026-09-09 07:47:00 +0700

    peringatan oli

--- resources/views/layouts/navbars/auth/nav.blade.php
+++ resources/views/layouts/navbars/auth/nav.blade.php
@@ -6,7 +6,7 @@
     $oliStatus = null;
     if ($totalGilingKotor > 90000) {
         $oliStatus = 'danger';
-    } elseif ($totalGilingKotor > 80000) {
+    } elseif ($totalGilingKotor > 800000) {
         $oliStatus = 'warning';
     }
 @endphp
